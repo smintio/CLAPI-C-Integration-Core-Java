e8a65b5be6a76bb424618bdb09208317ebdea3cf
Nikolaus Rosenmayr <nikolaus@smint.io>
2019-11-27 02:29:36 +0100

    feature: (doc) add package-info for authenticator module

---
+++ smintio-clapi-c-integration-authorizer/src/main/java/io/smint/clapi/consumer/integration/core/authenticator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <p>
+ * <a href="https://www.smint.io">Smint.io</a> additional authenticator library for the downstream library, containins
+ * helper classes to make OAuth 2.0 authentication and authorization with Smint.io API a breeze.
+ * </p>
+ */
+package io.smint.clapi.consumer.integration.core.authenticator;
